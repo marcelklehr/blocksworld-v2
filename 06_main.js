47b379a46f9d97a6830afe8f2602b42105a92ed8
bgrusdt <grusdt@uni-potsdam.de>
2020-10-05 20:21:23 +0200

    updated to Prolific mode

--- 06_main.js
+++ 06_main.js
@@ -42,7 +42,7 @@ $("document")
         // Possible deployment methods are:
         // "debug" and "directLink"
         // As well as "MTurk", "MTurkSandbox" and "Prolific"
-        deployMethod: "debug",
+        deployMethod: "Prolific",
         contact_email: "britta.grusdt@uni-osnabrueck.de",
         prolificURL: "https://app.prolific.co/submissions/complete?cc=577DD4C6"
       },
